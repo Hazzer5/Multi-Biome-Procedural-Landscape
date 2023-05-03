e1b739f3f8ef0461e275b58ab536789af8b88b5b
Hazzer5 <harrygbrettell4@gmail.com>
2023-05-03 16:01:55 +0100

    Created the treading controller

---
+++ Assets/Scripts/ThreadedDataRequester.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System;
+
+public class ThreadedDataRequester : MonoBehaviour
+{
+    static ThreadedDataRequester instance;
+    Queue<ThreadInfo> DataThreadInfoQueue = new Queue<ThreadInfo>();
+
+    void Awake() {
+        instance = FindObjectOfType<ThreadedDataRequester> ();
+    }
+
+    public static void RequestData(Func<object> dataGenerator, Action<object> callback) {
+        ThreadStart threadStart = delegate {
+            instance.DataThread (dataGenerator, callback);
+        };
+
+        new Thread (threadStart).Start ();
+    }
+
+    void DataThread(Func<object> dataGenerator, Action<object> callback) {
+        object data = dataGenerator();
+        lock (DataThreadInfoQueue) {
+            DataThreadInfoQueue.Enqueue(new ThreadInfo (callback, data));
+        }
+    }
+
+
+    void Update() {
+        lock (DataThreadInfoQueue) {
+            if (DataThreadInfoQueue.Count > 0) {
+                for (int i = 0; i < DataThreadInfoQueue.Count; i++) {
+                    ThreadInfo threadInfo = DataThreadInfoQueue.Dequeue ();
+                    threadInfo.callback(threadInfo.parameter);
+                }
+            }
+        }
+    }
+
+    
+    
+    struct ThreadInfo {
+        public readonly Action<object> callback;
+        public readonly object parameter;
+
+        public ThreadInfo (Action<object> callback, object parameter) {
+            this.callback = callback;
+            this.parameter = parameter;
+        }
+    }
+}
